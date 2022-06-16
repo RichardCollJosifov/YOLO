72a977a30e6fc43f359991bba14fde9dfdfeb32a
RichardCollJosifov <54583842+RichardCollJosifov@users.noreply.github.com>
2022-06-16 23:54:05 +0200

    Create README.m

---
+++ NetworkTraining/Models/README.m
@@ -0,0 +1,5 @@
+The following YOLOv4 and tiny-YOLOv4 models, trained on a sample of the VisDrone dataset, can be found:
+- tiny-YOLOv4 with input of size 416x416
+- tiny-YOLOv4 with input of size 640x640
+- YOLOv4 with input of size 480x480
+- YOLOv4 with input of size 640x640
